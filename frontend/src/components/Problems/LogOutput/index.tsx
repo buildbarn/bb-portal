5703523a4a4659a7640fd3e1030edfe664833c5c
Trey Ivy <trey@stackav.com>
2024-11-13 18:42:22 +0000

    remove comments

--- frontend/src/components/Problems/LogOutput/index.tsx
+++ frontend/src/components/Problems/LogOutput/index.tsx
@@ -21,22 +21,6 @@ const DEFAULT_TAIL_BYTES = 10_000;
 
 /* eslint-disable consistent-return */
 const LogOutput: React.FC<Props> = ({blobReference, tailBytes = DEFAULT_TAIL_BYTES}) => {
-    // if (blobReference.ephemeralURL != "" && env('NEXT_PUBLIC_BROWSER_URL')) {
-    //     const url = new URL(blobReference.ephemeralURL, env('NEXT_PUBLIC_BROWSER_URL'))
-    //     var logDownload = <Space direction="horizontal" size="small">
-    //       <Card>
-    //         <ButtonGroup>
-    //           <Button>
-    //             <DownloadOutlined/>
-    //             <Link href={url.toString()} download="test.log" target="_self">Download Log File</Link>
-    //           </Button>
-    //           <Tooltip title="Depending on the configuration of your remote cache, this link may point to ephemeral content and it could disappear.">
-    //             <Button icon={<WarningOutlined/>} danger/>
-    //           </Tooltip>
-    //         </ButtonGroup>
-    //       </Card>
-    //     </Space>
-    //   }
     const [contents, setContents] = useState<string>("");
     useEffect(() => {
         fetch(blobReference.downloadURL, {
