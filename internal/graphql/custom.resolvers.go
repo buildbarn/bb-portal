f63f4f54795f251ab3d7ddf0e59e692a26e6bc99
Trey Ivy <trey@stackav.com>
2025-02-10 19:53:18 +0000

    make lint

--- internal/graphql/custom.resolvers.go
+++ internal/graphql/custom.resolvers.go
@@ -382,7 +382,7 @@ func (r *queryResolver) GetTargetPassAggregation(ctx context.Context, label *str
 }
 
 // GetTestsWithOffset is the resolver for the getTestsWithOffset field.
-func (r *queryResolver) GetTestsWithOffset(ctx context.Context, label *string, offset *int, limit *int, sortBy *string, direction *string) (*model.TestGridResult, error) {
+func (r *queryResolver) GetTestsWithOffset(ctx context.Context, label *string, offset, limit *int, sortBy, direction *string) (*model.TestGridResult, error) {
 	maxLimit := 100
 	take := 10
 	skip := 0
@@ -425,7 +425,7 @@ func (r *queryResolver) GetTestsWithOffset(ctx context.Context, label *string, o
 }
 
 // GetTargetsWithOffset is the resolver for the GetTargetsWithOffset field.
-func (r *queryResolver) GetTargetsWithOffset(ctx context.Context, label *string, offset *int, limit *int, sortBy *string, direction *string) (*model.TargetGridResult, error) {
+func (r *queryResolver) GetTargetsWithOffset(ctx context.Context, label *string, offset, limit *int, sortBy, direction *string) (*model.TargetGridResult, error) {
 	maxLimit := 100
 	take := 10
 	skip := 0
@@ -514,7 +514,9 @@ func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
 // TestResult returns TestResultResolver implementation.
 func (r *Resolver) TestResult() TestResultResolver { return &testResultResolver{r} }
 
-type actionProblemResolver struct{ *Resolver }
-type blobReferenceResolver struct{ *Resolver }
-type mutationResolver struct{ *Resolver }
-type testResultResolver struct{ *Resolver }
+type (
+	actionProblemResolver struct{ *Resolver }
+	blobReferenceResolver struct{ *Resolver }
+	mutationResolver      struct{ *Resolver }
+	testResultResolver    struct{ *Resolver }
+)
