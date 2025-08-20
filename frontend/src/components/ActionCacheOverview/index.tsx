893731a4d462278dc1f168441307959d3b8b14fc
Isak Stenström <isak@meroton.com>
2025-08-20 17:23:51 +0200

    Change colors for the Action Cache Overview graph

--- frontend/src/components/ActionCacheOverview/index.tsx
+++ frontend/src/components/ActionCacheOverview/index.tsx
@@ -2,16 +2,19 @@ import type {
   ActionCacheStatistics,
   Maybe,
 } from "@/graphql/__generated__/graphql";
-import { Space, Statistic, Tooltip } from "antd";
+import { Space, Statistic } from "antd";
 import { Bar, BarChart, LabelList, Legend } from "recharts";
+import { YAxis } from "recharts";
 import { nullPercent } from "../Utilities/nullPercent";
 
 interface Props {
   acStatistics?: Maybe<ActionCacheStatistics>;
 }
 
-const HIT_COLOR = "#82ca9d";
-const MISS_COLOR = "#8884d8";
+const MISS_COLOR = "#777777";
+const HIT_COLOR = "#49AA19";
+const LABBEL_COLOR = "#000000";
+const LABEL_THRESHOLD = 0.05;
 
 const ActionCacheOverview: React.FC<Props> = ({ acStatistics }) => {
   const hitMissTotal = (acStatistics?.hits ?? 0) + (acStatistics?.misses ?? 0);
@@ -35,12 +38,25 @@ const ActionCacheOverview: React.FC<Props> = ({ acStatistics }) => {
         margin={{ top: 0, left: 10, bottom: 10, right: 10 }}
       >
         <Bar dataKey="Miss" fill={MISS_COLOR} stackId="a">
-          <LabelList dataKey="miss_label" position="center" />
+          {(acStatistics?.misses ?? 0) / hitMissTotal >= LABEL_THRESHOLD && (
+            <LabelList
+              dataKey="miss_label"
+              position="center"
+              fill={LABBEL_COLOR}
+            />
+          )}
         </Bar>
         <Bar dataKey="Hit" fill={HIT_COLOR} stackId="a">
-          <LabelList dataKey="hit_label" position="center" />
+          {(acStatistics?.hits ?? 0) / hitMissTotal >= LABEL_THRESHOLD && (
+            <LabelList
+              dataKey="hit_label"
+              position="center"
+              fill={LABBEL_COLOR}
+            />
+          )}
         </Bar>
-        <Tooltip />
+        {/* YAxis is hidden, but needs to exist to force the correct domain for the chart */}
+        <YAxis domain={[0, hitMissTotal]} hide={true} />
         <Legend />
       </BarChart>
       <Statistic
