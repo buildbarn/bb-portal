506350d5cd5bd540ac78bf6ae6cf668a6d2c61a4
Isak Stenström <isak@meroton.com>
2025-10-07 10:06:34 +0200

    Write a buffer based log normalizer
    Writes a log normalizer that operates on a mutable buffer. This allows
    a simpler more robust solution.

--- internal/api/grpc/bes/utils.go
+++ internal/api/grpc/bes/utils.go
@@ -1,8 +1,8 @@
 package bes
 
 import (
+	"bytes"
 	"context"
-	"strings"
 
 	"github.com/buildbarn/bb-portal/ent/gen/ent"
 	"github.com/buildbarn/bb-storage/pkg/util"
@@ -17,26 +17,53 @@ func GetNormalizedIncompleteBuildLogs(ctx context.Context, invocation *ent.Bazel
 	if err != nil {
 		return "", util.StatusWrap(err, "Failed to query incomplete build logs from database")
 	}
-
-	var sb strings.Builder
+	// Read all snippets into one big buffer.
+	var buf bytes.Buffer
 	for _, logSnippet := range incompleteLogs {
-		sb.WriteString(logSnippet.LogSnippet)
-	}
-
-	result := []string{}
-	for _, line := range strings.Split(sb.String(), "\n") {
-		for {
-			// ANSI escape sequence for removing the previous line
-			index := strings.Index(line, "\r\x1b[1A\x1b[K")
-			if index == -1 {
-				break
-			}
-			line = line[index+8:]
-			if len(result) > 0 {
-				result = result[:len(result)-1]
-			}
+		_, err := buf.WriteString(logSnippet.LogSnippet)
+		if err != nil {
+			return "", util.StatusWrap(err, "Could not write to an in memory buffer, is your computer on fire?")
 		}
-		result = append(result, line)
+
 	}
-	return strings.Join(result, "\n"), nil
+	// Determine which lines to keep and where they start/end.
+	var lineSpans [][2]int
+	data := buf.Bytes()
+	size := len(data)
+	current := 0
+	ansi := []byte("\r\x1b[1A\x1b[K")
+	for current < size {
+		start, end := current, 0
+		endOffset := bytes.IndexByte(data[current:], '\n')
+		var line []byte
+		if endOffset == -1 {
+			line = data[start:]
+			end = size
+		} else {
+			// Include the \n in the line.
+			end = start + endOffset + 1
+			line = data[start:end]
+		}
+		current = end
+		// Remove any earlier lines
+		count := bytes.Count(line, ansi)
+		if count > 0 {
+			if len(lineSpans) >= count {
+				lineSpans = lineSpans[:len(lineSpans)-count]
+			} else {
+				lineSpans = lineSpans[:0]
+			}
+			start = start + bytes.LastIndex(line, ansi) + len(ansi)
+		}
+		lineSpans = append(lineSpans, [2]int{start, end})
+	}
+	// Write all lines to keep to the beginning of the buffer.
+	cursor := 0
+	for _, span := range lineSpans {
+		start, end := span[0], span[1]
+		copy(data[cursor:], data[start:end])
+		cursor += end - start
+	}
+	// Return resulting string.
+	return string(data[:cursor]), nil
 }
