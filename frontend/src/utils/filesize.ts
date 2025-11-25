a1c7488f365596b7a0aa4742afcb296d840dfc4d
Isak Stenström <isak@meroton.com>
2025-11-25 09:40:06 +0100

    Don't format file sizes with scientific notation

--- frontend/src/utils/filesize.ts
+++ frontend/src/utils/filesize.ts
@@ -3,23 +3,23 @@ export const readableFileSize = (sizeBytes: number, precision = 3): string => {
     return `${sizeBytes} B`;
   }
 
-  const kb = sizeBytes / 1024;
-  if (kb < 1024) {
-    return `${kb.toPrecision(precision)}kiB`;
+  const kib = sizeBytes / 1024;
+  if (kib < 1024) {
+    return `${Number(kib.toPrecision(precision))} kiB`;
   }
 
-  const mb = kb / 1024;
-  if (mb < 1024) {
-    return `${mb.toPrecision(precision)}MiB`;
+  const mib = kib / 1024;
+  if (mib < 1024) {
+    return `${Number(mib.toPrecision(precision))} MiB`;
   }
 
-  const gb = mb / 1024;
-  if (gb < 1024) {
-    return `${gb.toPrecision(precision)}GiB`;
+  const gib = mib / 1024;
+  if (gib < 1024) {
+    return `${Number(gib.toPrecision(precision))} GiB`;
   }
 
-  const tb = gb / 1024;
-  return `${tb.toPrecision(precision)}TiB`;
+  const tib = gib / 1024;
+  return `${Number(tib.toPrecision(precision))} TiB`;
 };
 
 export const readableFileSizeFromString = (
