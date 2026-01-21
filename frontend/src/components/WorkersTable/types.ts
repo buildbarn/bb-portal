609c3b347051f36fcda5099ca17e72a11260cd10
Isak Stenström <isak@meroton.com>
2026-01-21 10:40:48 +0100

    Remove unused frontend files

--- frontend/src/components/WorkersTable/types.ts
+++
@@ -1,6 +0,0 @@
-import type { PlatformQueueState } from "@/lib/grpc-client/buildbarn/buildqueuestate/buildqueuestate";
-
-export interface PlatformQueueTableState extends PlatformQueueState {
-  numberOfSizeClasses: number;
-  isFirstSizeClass: boolean;
-}
