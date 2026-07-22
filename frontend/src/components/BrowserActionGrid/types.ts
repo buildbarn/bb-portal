ce8b26d232f6fe89daf462e7c07f5015878577e8
Isak Stenström <isak@meroton.com>
2026-07-22 16:44:04 +0200

    Remove unused frontend files

--- frontend/src/components/BrowserActionGrid/types.ts
+++
@@ -1,9 +0,0 @@
-import type { Digest } from "@/lib/grpc-client/build/bazel/remote/execution/v2/remote_execution";
-
-export type ActionConsoleOutput = {
-  name: string;
-  digest: Digest | undefined;
-  tooLarge: boolean;
-  notFound: boolean;
-  content: string | undefined;
-};
