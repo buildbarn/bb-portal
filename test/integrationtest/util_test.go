57cd49de2410d987ada960501900f8ff5a07e436
Richard Lavoie <lavoie.richard@gmail.com>
2026-08-02 22:04:45 -0400

    Ensure embedded PostgreSQL test cleanup runs

--- test/integrationtest/util_test.go
+++ test/integrationtest/util_test.go
@@ -33,7 +33,7 @@ func TestMain(m *testing.M) {
 		os.Exit(1)
 	}
 	defer dbProvider.Cleanup()
-	os.Exit(m.Run())
+	m.Run()
 }
 
 func setupTestBepUploader(t *testing.T, db database.Client, testCase testCase) *bepuploader.BepUploader {
